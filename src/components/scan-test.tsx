499450743f4b37d85c7b8a9ae104b5bccc30456d
amish <amish@qrforge.local>
2026-06-27 00:13:14 +0530

    Fix strict ESLint unused param issue

--- src/components/scan-test.tsx
+++ src/components/scan-test.tsx
@@ -31,7 +31,7 @@ export function ScanTest() {
           }
           setIsScanning(false);
         },
-        (_error) => {
+        () => {
           // ignore errors silently while scanning
         }
       );
